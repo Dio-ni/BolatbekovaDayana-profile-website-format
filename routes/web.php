2bf632676dbb0c6d2e5a1140296cf8c013c2854b
Dayana Bolatbekova <200103264@stu.sdu.edu.kz>
2021-03-15 20:54:23 +0600

    add controllers and index, create views

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
 use App\Models\Post;
+use App\Http\Controllers\BlogController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -40,3 +41,13 @@ Route::get('/​post', function () {
     $post = Post::find(1);
     return $post;
 });
+
+//Controllers
+
+Route::get('blog', [BlogController::class,'index']);
+
+Route::get('blog/create', function(){
+    return view('blog.create');
+});
+
+Route::post('blog/create', [BlogController::class, 'store'])->name('add-blog');
